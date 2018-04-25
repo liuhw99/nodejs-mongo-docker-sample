3c77b0895b39cc8f1401ea031bdfd5c8cf39d72b
liuhu <Huawei.Liu@pointclickcare.com>
2018-04-25 13:09:51 -0400

    local dev

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
-FROM node:boron
+FROM node:latest
 
 WORKDIR /usr/src/app
-
+RUN npm install -g nodemon
 COPY package.json /usr/src/app
 
-COPY package-lock.json /usr/src/app
-
 RUN npm install
-
-RUN npm install pm2 -g
 
 COPY . /usr/src/app
 
 EXPOSE 3000
 
-CMD ["pm2-docker", "start", "process.json"]
+CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var PORT = 3000;
-
-// REQUIRE MIDDLEWARE
 var instantMongoCrud = require('express-mongo-crud'); // require the module
 
-mongoose.connect('database:27017/mongocrud');
+var HTTP_PORT = 3000;
+var HTTP_HOST = "localhost";//"192.168.99.100";
+var DB_HOST = "192.168.99.100";
+
+// REQUIRE MIDDLEWARE
+mongoose.connect(DB_HOST + ':27017/mongocrud');
+app.use(bodyParser.json()); // add body parser
 
 var options = { //specify express-mongo-crud options
-	host: `localhost:${PORT}`
+	host: HTTP_HOST + ":" + HTTP_PORT
 }
-
-//USE AS MIDDLEWARE
-app.use(bodyParser.json()); // add body parser
 app.use(instantMongoCrud(options)); // use as middleware
 
-app.listen(PORT, ()=>{
+router.get('/', function(req, res){
+    res.send('works well112244');
+});
+app.use(router);
+
+app.listen(HTTP_PORT, ()=>{
 	console.log('started');
 });
